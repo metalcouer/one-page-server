2c5f18712640980a255087a960c6e7ea5322659b
Diane Sanchez <diane@Dianes-MacBook-Air-2.local>
2019-01-15 14:13:57 -0700

    first commit added npm, server works

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const listener = () => console.log(`Application listening on port ${port}!`)
+const listener = () => console.log(`Party on port ${port}!`)
 app.listen(port, listener)
